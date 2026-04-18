61ae2de6b3bf0a754d8e6399b4d34f620364006e
WildRelation <none@none>
2026-04-18 15:34:47 +0200

    Restore configurable POSTGRES_PORT variable

--- java-api/src/main/java/se/kth/ducklake/service/DuckLakeService.java
+++ java-api/src/main/java/se/kth/ducklake/service/DuckLakeService.java
@@ -11,6 +11,7 @@ import java.util.*;
 public class DuckLakeService {
 
     @Value("${ducklake.postgres.host}")     private String pgHost;
+    @Value("${ducklake.postgres.port}")     private String pgPort;
     @Value("${ducklake.postgres.db}")       private String pgDb;
     @Value("${ducklake.postgres.user}")     private String pgUser;
     @Value("${ducklake.postgres.password}") private String pgPass;
@@ -44,11 +45,11 @@ public class DuckLakeService {
                 CREATE OR REPLACE SECRET (
                     TYPE postgres,
                     HOST '%s',
-                    PORT 5432,
+                    PORT %s,
                     DATABASE '%s',
                     USER '%s',
                     PASSWORD '%s'
-                )""".formatted(pgHost, pgDb, pgUser, pgPass));
+                )""".formatted(pgHost, pgPort, pgDb, pgUser, pgPass));
 
             String dataPath;
             if (!s3Endpoint.isBlank()) {
